f58f3552bddb162824284de9d9b35dd97b0831c6
tokyosuperch <supachan@sfc.wide.ad.jp>
2020-11-04 13:54:46 +0900

    最初のコミット

---
+++ main.js
@@ -0,0 +1,75 @@
+main();
+
+async function main() {
+  var head = document.getElementById("head");
+  var i2cAccess = await navigator.requestI2CAccess();
+  var port = i2cAccess.ports.get(1);
+  var pca9685 = new PCA9685(port, 0x40);
+  var angle = 0;
+  // console.log("angle"+angle);
+  // servo setting for sg90
+  // Servo PWM pulse: min=0.0011[sec], max=0.0019[sec] angle=+-60[deg]
+  await pca9685.init(0.001, 0.002, 30);
+  for (;;) {
+    angle = angle <= -30 ? 30 : -30;
+    // console.log("angle"+angle);
+    await pca9685.setServo(0, angle);
+    await pca9685.setServo(1, angle);
+    // console.log('value:', angle);
+    head.innerHTML = angle;
+    await sleep(1000);
+  }
+}
+
+window.onload = function() {
+  document.body.onclick = init
+}
+const init = async function() {
+  document.body.onclick = null
+
+  let fftsize = 15
+  if (document.location.hash.length > 1) {
+    fftsize = parseInt(document.location.hash.substring(1))
+  }
+  if (fftsize < 5) {
+    fftsize = 5
+  } else if (fftsize > 15) {
+    fftsize = 15
+  }
+
+  if (!navigator.mediaDevices.getUserMedia) {
+    alert('getUserMedia not supported')
+    return
+  }
+  const audio = new (window.AudioContext || window.webkitAudioContext)()
+  const stream = await navigator.mediaDevices.getUserMedia({ audio: true })
+  const source = audio.createMediaStreamSource(stream)
+  const analyser = audio.createAnalyser() // FFT
+  //analyser.minDecibels = -90
+  //analyser.maxDecibels = -10
+  analyser.smoothingTimeConstant = 0 // 0-.999 0.85 ふわっとなる
+  analyser.fftSize = 1 << fftsize // min:32==1<<5 max:32768==1<<15
+  source.connect(analyser)
+
+  console.log(audio.sampleRate) // 44100 固定?
+  console.log(audio.sampleRate / analyser.fftSize + "Hz") // 分解能
+
+  const buflen = analyser.frequencyBinCount
+  const buf = new Uint8Array(buflen)
+
+  var text = document.getElementById("peak");
+  const draw = function() {
+    analyser.getByteFrequencyData(buf)
+    var peak = [0,0];
+    for (let i = 0; i < buflen; i++) {
+      const n = buf[i]
+      if (peak[0] < n) {
+        peak[0] = n;
+        peak[1] = i;
+      }
+    }
+    requestAnimationFrame(draw);
+    text.innerHTML = Math.round(peak[1] * audio.sampleRate / analyser.fftSize) + "Hz<br />大きさ:" + ( '000' + peak[0] ).slice( -3 );;
+  }
+  draw()
+}
